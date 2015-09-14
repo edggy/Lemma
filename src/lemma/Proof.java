5217e881e19b8750105fd59c80fc2df438edb2ee
edggy <edggy@Ethan-Alienware>
2015-09-13 20:20:47 -0400

    Made Proof class

---
+++ src/lemma/Proof.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package lemma;
+
+/**
+ * @author Ethan
+ *
+ */
+public class Proof {
+	
+	/**
+	 * Proof verifier
+	 * @param inferences The Inferences to use to verify the proof
+	 * @param settings The Settings to use to verify the proof
+	 * @return Whether the proof is valid based on the Inferences and Settings
+	 */
+	public boolean verify(Inferences inferences, Settings settings) {
+		//TODO
+		return false;
+	}
+}
